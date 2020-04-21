c76a918e9872712a83ab98e457372de440c9c17c
EnriqueDoster <12348747+EnriqueDoster@users.noreply.github.com>
2020-04-21 16:47:53 -0600

    Add files via upload

--- scripts/print_AMR_figures.R
+++ scripts/print_AMR_figures.R
@@ -1,5 +1,22 @@
 ## Print out some exploratory figures 
 
+######################################################
+## Exploratory Analyses: Alpha Diversity CSS values ##
+######################################################
+for( v in 1:length(AMR_exploratory_analyses) ) {
+  # AMR
+  meg_alpha_diversity(data_list=AMR_analytic_data,
+                      data_names=AMR_analytic_names,
+                      metadata=metadata,
+                      sample_var=sample_column_id,
+                      group_var=AMR_exploratory_analyses[[v]]$exploratory_var,
+                      analysis_subset=AMR_exploratory_analyses[[v]]$subsets,
+                      outdir=paste(graph_output_dir, 'AMR', AMR_exploratory_analyses[[v]]$name,
+                                   sep='/', collapse=''),
+                      data_type='AMR',
+                      factor_order= AMR_exploratory_analyses[[v]]$order)
+}
+
 
 #############################################
 ## Exploratory Analyses: Alpha Rarefaction ##
@@ -14,8 +31,10 @@ for( v in 1:length(AMR_exploratory_analyses) ) {
                         analysis_subset=AMR_exploratory_analyses[[v]]$subsets,
                         outdir=paste(graph_output_dir, 'AMR', AMR_exploratory_analyses[[v]]$name,
                                      sep='/', collapse=''),
-                        data_type='AMR')
+                        data_type='AMR',
+                        factor_order= AMR_exploratory_analyses[[v]]$order)
 }
+
 
 ######################################
 ## Exploratory Analyses: Ordination ##
@@ -31,7 +50,8 @@ for( v in 1:length(AMR_exploratory_analyses) ) {
                  outdir = paste(graph_output_dir, 'AMR', AMR_exploratory_analyses[[v]]$name,
                                 sep='/', collapse=''),
                  data_type = 'AMR',
-                 method = 'NMDS')
+                 method = 'NMDS',
+                 factor_order= AMR_exploratory_analyses[[v]]$order)
   
   # AMR PCA
   meg_ordination(data_list = AMR_analytic_data,
@@ -43,8 +63,10 @@ for( v in 1:length(AMR_exploratory_analyses) ) {
                  outdir = paste(graph_output_dir, 'AMR', AMR_exploratory_analyses[[v]]$name,
                                 sep='/', collapse=''),
                  data_type = 'AMR',
-                 method = 'PCA')
+                 method = 'PCA',
+                 factor_order= AMR_exploratory_analyses[[v]]$order)
 }
+
 
 ####################################
 ## Exploratory Analyses: Heatmaps ##
@@ -61,10 +83,55 @@ for( v in 1:length(AMR_exploratory_analyses) ) {
                 analysis_subset=AMR_exploratory_analyses[[v]]$subsets,
                 outdir=paste(graph_output_dir, 'AMR', AMR_exploratory_analyses[[v]]$name,
                              sep='/', collapse=''),
-                data_type='AMR')
+                data_type='AMR',
+                factor_order= AMR_exploratory_analyses[[v]]$order)
   }
 }
 
+#######################################################
+## Exploratory Analyses: Median abundance Barplots ##
+#######################################################
+
+for( v in 1:length(AMR_exploratory_analyses) ) {
+  for( l in 1:length(AMR_analytic_names) ) {
+    suppressWarnings(
+      meg_median_barplot(melted_data=amr_melted_analytic,
+                         metadata=metadata,
+                         sample_var=sample_column_id,
+                         group_var=AMR_exploratory_analyses[[v]]$exploratory_var,
+                         level_var=AMR_analytic_names[l],
+                         analysis_subset=AMR_exploratory_analyses[[v]]$subsets,
+                         outdir=paste(graph_output_dir, 'AMR', AMR_exploratory_analyses[[v]]$name,
+                                      sep='/', collapse=''),
+                         data_type='AMR',
+                         factor_order= AMR_exploratory_analyses[[v]]$order)
+    )
+  }
+}
+
+
+#######################################################
+## Exploratory Analyses: Relative abundance Barplots ##
+#######################################################
+
+
+# AMR
+for( v in 1:length(AMR_exploratory_analyses) ) {
+  for( l in 1:length(AMR_analytic_names) ) {
+    suppressWarnings(
+      meg_relative_barplot(melted_data=amr_melted_analytic,
+                           metadata=metadata,
+                           sample_var=sample_column_id,
+                           group_var=AMR_exploratory_analyses[[v]]$exploratory_var,
+                           level_var=AMR_analytic_names[l],
+                           analysis_subset=AMR_exploratory_analyses[[v]]$subsets,
+                           outdir=paste(graph_output_dir, 'AMR', AMR_exploratory_analyses[[v]]$name,
+                                        sep='/', collapse=''),
+                           data_type='AMR',
+                           factor_order= AMR_exploratory_analyses[[v]]$order)
+    )
+  }
+}
 
 
 ####################################
@@ -83,10 +150,51 @@ for( v in 1:length(AMR_exploratory_analyses) ) {
                   analysis_subset=AMR_exploratory_analyses[[v]]$subsets,
                   outdir=paste(graph_output_dir, 'AMR', AMR_exploratory_analyses[[v]]$name,
                                sep='/', collapse=''),
-                  data_type='AMR')
+                  data_type='AMR',
+                  factor_order= AMR_exploratory_analyses[[v]]$order)
     )
   }
 }
+
+
+
+##########################
+## Statistical Analyses ##
+##########################
+# for( a in 1:length(AMR_statistical_analyses) ) {
+#   meg_fitZig(data_list=AMR_analytic_data,
+#              data_names=AMR_analytic_names,
+#              metadata=metadata,
+#              zero_mod=model.matrix(~1 + log(libSize(amr))),
+#              data_mod=AMR_statistical_analyses[[a]]$model_matrix,
+#              filter_min_threshold=0.15,
+#              contrast_list=AMR_statistical_analyses[[a]]$contrasts,
+#              random_effect_var=AMR_statistical_analyses[[a]]$random_effect,
+#              outdir=paste(stats_output_dir, 'AMR', AMR_statistical_analyses[[a]]$name,
+#                           sep='/', collapse=''),
+#              analysis_name=AMR_statistical_analyses[[a]]$name,
+#              analysis_subset=AMR_statistical_analyses[[a]]$subsets,
+#              data_type='AMR',
+#              pval=0.99,
+#              top_hits=1000)
+# }
+# for( a in 1:length(microbiome_statistical_analyses) ) {
+#   meg_fitZig(data_list=microbiome_analytic_data,
+#              data_names=microbiome_analytic_names,
+#              metadata=microbiome_metadata,
+#              zero_mod=model.matrix(~1 + log(libSize(microbiome))),
+#              data_mod=microbiome_statistical_analyses[[a]]$model_matrix,
+#              filter_min_threshold=0.15,
+#              contrast_list=microbiome_statistical_analyses[[a]]$contrasts,
+#              random_effect_var=microbiome_statistical_analyses[[a]]$random_effect,
+#              outdir=paste(stats_output_dir, 'Microbiome', microbiome_statistical_analyses[[a]]$name,
+#                           sep='/', collapse=''),
+#              analysis_name=microbiome_statistical_analyses[[a]]$name,
+#              analysis_subset=microbiome_statistical_analyses[[a]]$subsets,
+#              data_type='Microbiome',
+#              pval=0.99,
+#              top_hits=1000)
+# }
 
 
 ########################
@@ -113,4 +221,5 @@ write.csv(make_sparse(amr_norm, 'header', c('header', 'class', 'mechanism', 'gro
           row.names=T)
 write.table(amr_norm, 'amr_matrices/normalized/AMR_Gene_Normalized.csv', sep=',', row.names = F, col.names = T)
 write.table(amr_raw, 'amr_matrices/raw/AMR_Gene_Raw.csv', sep=',', row.names = F, col.names = T)
+
 
